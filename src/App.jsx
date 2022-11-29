0cdb99ff6c545b8c83a70cd2cc82ce876427430e
Junaid Ali <Junaid Ali>
2022-11-29 20:28:09 +0500

    offline fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
 import AppNav from "./Components/AppNav";
-import ContactUs from "./Components/ContactUs";
+import FeaturedProducts from "./Components/FeaturedProducts";
 import Footer from "./Components/Footer";
 import Hero from "./Components/Hero";
-import KeyFeatures from "./Components/KeyFeatures";
 import ProductCarousel from "./Components/ProductCarousel";
-import SingleProduct from "./Components/SingleProduct";
 import SocialSection from "./Components/SocialSection";
-
+import data from "./assets/data.json";
 const App = () => {
   const [images, setImages] = useState([]);
   useEffect(() => {
@@ -17,14 +15,13 @@ const App = () => {
   }, []);
 
   const loadImages = () => {
-    console.log("fetching");
-    fetch(
-      "https://api.unsplash.com/search/photos?page=1&query=product&client_id=B3eNrrOIeBFdr9pohQqaEPSXOYfuX-hacP2cNBcKY4s"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setImages(data.results);
-      });
+    // console.log("fetching");
+    // fetch(data)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setImages(data.results);
+    //   });
+    setImages(data.results);
   };
   return (
     <>
@@ -32,10 +29,15 @@ const App = () => {
         <AppNav></AppNav>
         <Hero images={images}></Hero>
       </div>
-      <ProductCarousel images={images}></ProductCarousel>
-      <KeyFeatures></KeyFeatures>
-      <SingleProduct></SingleProduct>
-      <ContactUs />
+      <ProductCarousel
+        images={images}
+        title={"Latest Products"}
+      ></ProductCarousel>
+      <FeaturedProducts images={images} />
+      <ProductCarousel
+        images={images.slice(2, 7)}
+        title={"Limited Sale"}
+      ></ProductCarousel>
       <SocialSection></SocialSection>
       <Footer />
     </>
--- src/Components/ProductCarousel.jsx
+++ src/Components/ProductCarousel.jsx
@@ -22,7 +22,7 @@ const responsive = {
   },
 };
 
-const ProductCarousel = ({ images }) => {
+const ProductCarousel = ({ images, title }) => {
   return (
     <>
       <section className="carousel-section my-5">
@@ -34,9 +34,7 @@ const ProductCarousel = ({ images }) => {
                 Slogun line Lorem, ipsum dolor. Lorem ipsum dolor sit amet
                 consectetur adipisicing elit. Error, laboriosam.
               </p> */}
-              <p className="my-4 carousel-title fw-bold underline">
-                Checkout Our latest products here
-              </p>
+              <p className="my-4 carousel-title fw-bold underline">{title}</p>
             </div>
           </div>
           <div className="row">
@@ -55,12 +53,13 @@ const ProductCarousel = ({ images }) => {
                       <img
                         className="card-img-top shadow"
                         src={image.urls.small}
-                        alt="Card image cap"
+                        alt={image.description || "helo image"}
                       />
                       <div className="card-body text-center">
                         <h5 className="card-title mt-3">
                           {image.description || "Product Title"}
                         </h5>
+                        <p className="fw-bold discounted-price">$20.00</p>
                         <p className="fw-bold">$10.00</p>
                       </div>
                     </div>
@@ -68,39 +67,6 @@ const ProductCarousel = ({ images }) => {
                 })}
               </Carousel>
             </div>
-          </div>
-        </div>
-      </section>
-      <section className="featured-products my-5">
-        <h1 className="text-center my-4">Featured Products</h1>
-        <div className="container">
-          <div className="row  text-center justify-content-center">
-            {images.slice(3, 6).map((image, index) => {
-              return (
-                <div key={index} className="col-md-4 mb-4">
-                  <div className="card shadow feature-product-card">
-                    <img
-                      className="card-img"
-                      src={image.urls.small}
-                      alt="Bologna"
-                    />
-                    <div className="card-img-overlay text-white d-flex flex-column justify-content-center">
-                      <h1 className="card-title">
-                        {image.description || "no description"}
-                      </h1>
-                      <h6 className="card-subtitle mb-2">
-                        Emilia-Romagna Region, Italy
-                      </h6>
-                      <div className="text-center">
-                        <a href="#" className="btn btn-warning buy-now-btn">
-                          Buy Now
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
           </div>
         </div>
       </section>
