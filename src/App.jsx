fa06b00ffa197329c226db572ef5e19f37139b0f
Junaid Ali <Junaid Ali>
2022-11-30 06:30:33 +0500

    Some Changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from "./Components/Hero";
 import ProductCarousel from "./Components/ProductCarousel";
 import SocialSection from "./Components/SocialSection";
 import data from "./assets/data.json";
+import ContactUs from "./Components/ContactUs";
 const App = () => {
   const [images, setImages] = useState([]);
   useEffect(() => {
@@ -38,6 +39,7 @@ const App = () => {
         images={images.slice(2, 7)}
         title={"Limited Sale"}
       ></ProductCarousel>
+      {/* <ContactUs /> */}
       <SocialSection></SocialSection>
       <Footer />
     </>
--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -1,54 +1,28 @@
 import React from "react";
 import mailImage from "../assets/contact-mail.png";
-
+import image from "../assets/jun.jpg";
 const ContactUs = () => {
   return (
     <>
-      <section className="contact-section my-5 py-5">
+      <div className="contact-section">
         <div className="container">
-          <div className="card shadow">
-            <div className="card-body">
-              <div className="row text-center py-5">
-                <div className="col-md-6 d-flex align-items-center justify-content-center">
-                  <img
-                    className="contact-image"
-                    src={mailImage}
-                    alt="contact-image"
-                  />
-                </div>
-                <div className="col-md-4">
-                  <h1>Get in touch</h1>
-                  <div className="input-box text-center">
-                    <input
-                      className="form-control contact-input"
-                      type="text"
-                      placeholder="Name"
-                    />
-                    <input
-                      className="form-control contact-input"
-                      type="text"
-                      placeholder="Email"
-                    />
-                    <input
-                      className="form-control contact-input"
-                      type="text"
-                      placeholder="Subject"
-                    />
-                    <textarea
-                      rows="6"
-                      className="form-control contact-input"
-                    ></textarea>
-                    <span className="shadow-input"></span>
-                    <button className="contact-btn mt-4 btn ">
-                      Submit Email
-                    </button>
-                  </div>
-                </div>
+          <div className="row justify-content-center">
+            <div className="col-md-6 image-col">
+              <img className="contact-image" src={image} alt="Contact-image" />
+            </div>
+            <div className="col-md-4 ">
+              <div className="form-group my-5">
+                <label className="form-label">Name</label>
+                <input
+                  type="text"
+                  className="form-control contact-input"
+                  placeholder="Enter your name"
+                />
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
     </>
   );
 };
